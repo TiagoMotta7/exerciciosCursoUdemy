b8b194d2e94632be0198b6ccc3a0ef8c3b1b37ee
Tiago Motta <tiago91@gmail.com>
2024-01-17 14:57:17 -0300

    Envio dos arquivos

---
+++ index.js
@@ -0,0 +1,9 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+console.log(alunos);  
+
+
+//alunos.shift(); Remove o primeiro elemento do Array
+//alunos.pop(); Remove um elemento no fim do Array
+//alunos.unshift('Fábio'); Adiciona um nome no começo do Array
+
+//alunos.push('Fábio'); Adiciona um nome no fim do Array
